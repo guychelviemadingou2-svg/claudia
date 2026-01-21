3b4e1cd5adf591e8e252d28009d4c1caf4eae7cd
unknown <guychelviemadingou2@gmail.comgit config --global user.name>
2026-01-21 23:21:27 +0100

    Add GitHub setup script

---
+++ github_setup.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Instructions pour mettre sur GitHub:
+echo.
+echo 1. Creez un nouveau depot sur https://github.com
+echo    Nom: cosmos-blog
+echo    Description: Blog interactif avec systeme d'interaction sociale - Django
+echo.
+echo 2. Copiez l'URL de votre depot (ex: https://github.com/USERNAME/cosmos-blog.git)
+echo.
+echo 3. Executez ces commandes:
+echo    git remote add origin https://github.com/USERNAME/cosmos-blog.git
+echo    git branch -M main
+echo    git push -u origin main
+echo.
+echo Votre projet est pret pour GitHub !
+pause
